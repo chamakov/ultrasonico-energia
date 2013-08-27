9563be92e163c63289eaf2db9592ddc17131e783
Juan Francisco <jfoa92@hotmail.com>
2013-08-27 12:43:44 -0500

    Update SerialUltra.ino

--- examples/SerialUltra/SerialUltra.ino
+++ examples/SerialUltra/SerialUltra.ino
@@ -1,16 +1,15 @@
-
 #include <Ultrasonico.h>
 
 Ultrasonico ultrasonico(3,2); //(trigPin,echoPin)
 
 void setup(){
-  ultrasonico.Begin(); //iniciamos el sensor
+  ultrasonico.Begin(); //ints the sensor
 }
 
 void loop(){
-  Serial.print(ultrasonico.Distancia(1)); //imprimimos la distancia 1 para cm y 2 para pulgadas
+  Serial.print(ultrasonico.Distancia(1)); //Serial print the distance
   Serial.print(" cm");
   Serial.println();
-  delay(100); //delay entre medicion y medicion, minimo 50ms
+  delay(100); //delay between meditions, 50ms minimum
 }
 
